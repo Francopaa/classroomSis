d7e3ad89b7dfcc9b936fdea4b90c4ce7c7494356
wGamba <fpa936@gmail.com>
2026-04-13 20:26:44 -0400

    Guardar la entrega, validar que no exista una previa y vincularla al alumno

--- app/models/entrega.py
+++ app/models/entrega.py
@@ -19,6 +19,18 @@ def get_entrega(alumno_id, tarea_id):
     cur.close()
     return entrega
 
+def get_entrega_by_id(entrega_id):
+    cur = mysql.connection.cursor()
+    cur.execute("""
+        SELECT e.*, u.nombre as alumno_nombre
+        FROM entregas e
+        JOIN usuarios u ON e.alumno_id = u.id
+        WHERE e.id = %s
+    """, (entrega_id,))
+    entrega = cur.fetchone()
+    cur.close()
+    return entrega
+
 def get_entregas_tarea(tarea_id):
     cur = mysql.connection.cursor()
     cur.execute("""
@@ -30,3 +42,20 @@ def get_entregas_tarea(tarea_id):
     entregas = cur.fetchall()
     cur.close()
     return entregas
+
+def get_alumnos_con_entrega(tarea_id, clase_id):
+    cur = mysql.connection.cursor()
+    cur.execute("""
+        SELECT u.id as alumno_id, u.nombre,
+               e.id as entrega_id, e.entregado_en,
+               c.nota, c.comentario
+        FROM clase_alumnos ca
+        JOIN usuarios u ON ca.alumno_id = u.id
+        LEFT JOIN entregas e ON e.alumno_id = u.id AND e.tarea_id = %s
+        LEFT JOIN calificaciones c ON c.entrega_id = e.id
+        WHERE ca.clase_id = %s
+        ORDER BY u.nombre ASC
+    """, (tarea_id, clase_id))
+    resultado = cur.fetchall()
+    cur.close()
+    return resultado
